96255e285ee18fa853128956c47cba81e0d3a77d
Jorge Torres <jor-mario@hotmail.com>
2023-07-12 22:39:45 -0500

    update showOrHide

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 function showOrHide() {
   let [nav] = document.getElementsByTagName("nav");
-  let numOfClass = nav.classList;
-  if (numOfClass < 1) {
-    nav.classList.add("active");
-    return;
-  }
-  nav.classList.remove("active");
+  nav.classList.toggle("active");
 }
